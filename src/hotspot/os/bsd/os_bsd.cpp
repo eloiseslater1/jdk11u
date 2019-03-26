047b91096a0286c98bc70cadfa1484ec508daeff
Kurt Miller <bsdkurt@gmail.com>
2019-03-25 21:01:30 -0400

    Revert --with-package-path addition
    Remove additional uses of PACKAGE_PATH added in bsd-port branch:
    * For os_bsd.cpp set DEFAULT_LIBPATH to correct defaults by OS. The
    prefix can be changed using patch & substitute in ports or an
    alternate method added back that doesn't use PACKAGE_PATH.
    * For iconv, set the make vars to /usr/local/ on OpenBSD temporarily
    until we switch to a more generally accepted method for setting up
    these variables.

--- src/hotspot/os/bsd/os_bsd.cpp
+++ src/hotspot/os/bsd/os_bsd.cpp
@@ -377,8 +377,10 @@ void os::init_system_properties_values() {
 #ifndef DEFAULT_LIBPATH
 #ifdef __APPLE__
   #define DEFAULT_LIBPATH "/lib:/usr/lib"
+#elif defined(__NetBSD__)
+  #define DEFAULT_LIBPATH "/usr/lib:/usr/pkg/lib"
 #else
-  #define DEFAULT_LIBPATH "/usr/lib:" PACKAGE_PATH "/lib"
+  #define DEFAULT_LIBPATH "/usr/lib:/usr/local/lib"
 #endif
 #endif
 
