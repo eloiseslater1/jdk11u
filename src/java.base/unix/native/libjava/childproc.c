278434f761002f2d63dd3c9b901e034cdbee4041
Kurt Miller <bsdkurt@gmail.com>
2019-05-31 10:37:21 -0400

    Use closefrom(2) on all BSD's execpt MACOSX

--- src/java.base/unix/native/libjava/childproc.c
+++ src/java.base/unix/native/libjava/childproc.c
@@ -62,7 +62,7 @@ isAsciiDigit(char c)
   return c >= '0' && c <= '9';
 }
 
-#if defined(__OpenBSD__)
+#if defined(_BSDONLY_SOURCE)
 /*
  * Quoting POSIX: "If a multi-threaded process calls fork(), the new
  * process shall contain a replica of the calling thread and its entire
@@ -90,7 +90,7 @@ closeDescriptors(void)
   #define DIR DIR64
   #define opendir opendir64
   #define closedir closedir64
-#elif defined(_ALLBSD_SOURCE)
+#elif defined(MACOSX)
   #define FD_DIR "/dev/fd"
   #define dirent64 dirent
   #define readdir64 readdir
@@ -138,7 +138,7 @@ closeDescriptors(void)
 
     return 1;
 }
-#endif /* __OpenBSD__ */
+#endif /* _BSDONLY_SOURCE */
 
 int
 moveDescriptor(int fd_from, int fd_to)
