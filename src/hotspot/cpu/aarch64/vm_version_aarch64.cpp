2fcd725bda5ab06bbf08f35b8d7b395a29b7507d
miki <mikael.urankar@gmail.com>
2019-07-30 19:27:43 +0200

    port to FreeBSD aarch64

--- src/hotspot/cpu/aarch64/vm_version_aarch64.cpp
+++ src/hotspot/cpu/aarch64/vm_version_aarch64.cpp
@@ -35,8 +35,12 @@
 #include OS_HEADER_INLINE(os)
 
 #ifndef BUILTIN_SIM
+#if defined (__linux__)
 #include <sys/auxv.h>
 #include <asm/hwcap.h>
+#elif defined (__FreeBSD__)
+#include <machine/elf.h>
+#endif
 #else
 #define getauxval(hwcap) 0
 #endif
@@ -167,6 +171,7 @@ void VM_Version::get_processor_features() {
     SoftwarePrefetchHintDistance &= ~7;
   }
 
+#if defined(__linux__)
   unsigned long auxv = getauxval(AT_HWCAP);
 
   char buf[512];
@@ -194,6 +199,11 @@ void VM_Version::get_processor_features() {
     }
     fclose(f);
   }
+#elif defined(__FreeBSD__)
+  char buf[512];
+  int cpu_lines = 0;
+  unsigned long auxv = os_get_processor_features();
+#endif
 
   // Enable vendor specific features
 
